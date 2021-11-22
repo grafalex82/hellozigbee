7cd6429451c538a5707e0fe31520164732f474d3
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2021-11-22 09:59:14 +0200

    Improve log indentation while printing OTA header

--- DumpFunctions.cpp
+++ DumpFunctions.cpp
@@ -451,14 +451,14 @@ void vDumpCurrentImageOTAHeader(uint8 otaEp)
 {
     tsOTA_ImageHeader sOTAHeader;
     eOTA_GetCurrentOtaHeader(otaEp, FALSE, &sOTAHeader);
-    DBG_vPrintf(TRUE, "\n\nCurrent Image Details \n");
-    DBG_vPrintf(TRUE, "File ID = 0x%08x\n",sOTAHeader.u32FileIdentifier);
-    DBG_vPrintf(TRUE, "Header Ver ID = 0x%04x\n",sOTAHeader.u16HeaderVersion);
-    DBG_vPrintf(TRUE, "Header Length ID = 0x%04x\n",sOTAHeader.u16HeaderLength);
-    DBG_vPrintf(TRUE, "Header Control Field = 0x%04x\n",sOTAHeader.u16HeaderControlField);
-    DBG_vPrintf(TRUE, "Manufac Code = 0x%04x\n",sOTAHeader.u16ManufacturerCode);
-    DBG_vPrintf(TRUE, "Image Type = 0x%04x\n",sOTAHeader.u16ImageType);
-    DBG_vPrintf(TRUE, "File Ver = 0x%08x\n",sOTAHeader.u32FileVersion);
-    DBG_vPrintf(TRUE, "Stack Ver = 0x%04x\n",sOTAHeader.u16StackVersion);
-    DBG_vPrintf(TRUE, "Image Len = 0x%08x\n\n\n",sOTAHeader.u32TotalImage);
+    DBG_vPrintf(TRUE, "\nCurrent Image Details \n");
+    DBG_vPrintf(TRUE, "  File ID = 0x%08x\n",sOTAHeader.u32FileIdentifier);
+    DBG_vPrintf(TRUE, "  Header Ver ID = 0x%04x\n",sOTAHeader.u16HeaderVersion);
+    DBG_vPrintf(TRUE, "  Header Length ID = 0x%04x\n",sOTAHeader.u16HeaderLength);
+    DBG_vPrintf(TRUE, "  Header Control Field = 0x%04x\n",sOTAHeader.u16HeaderControlField);
+    DBG_vPrintf(TRUE, "  Manufac Code = 0x%04x\n",sOTAHeader.u16ManufacturerCode);
+    DBG_vPrintf(TRUE, "  Image Type = 0x%04x\n",sOTAHeader.u16ImageType);
+    DBG_vPrintf(TRUE, "  File Ver = 0x%08x\n",sOTAHeader.u32FileVersion);
+    DBG_vPrintf(TRUE, "  Stack Ver = 0x%04x\n",sOTAHeader.u16StackVersion);
+    DBG_vPrintf(TRUE, "  Image Len = 0x%08x\n\n",sOTAHeader.u32TotalImage);
 }
