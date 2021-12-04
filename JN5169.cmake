67e71b2149b5fe430774fc68a0f81694ce59361f
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2021-12-04 17:43:49 +0200

    Burn OTA identification string into OTA header

--- JN5169.cmake
+++ JN5169.cmake
@@ -102,7 +102,7 @@ FUNCTION(ADD_HEX_BIN_TARGETS TARGET)
     ADD_CUSTOM_TARGET("${TARGET}.bin"
         DEPENDS ${TARGET}
         COMMAND ${CMAKE_OBJCOPY} -j .version -j .bir -j .flashheader -j .vsr_table -j .vsr_handlers -j .rodata -j .text -j .data -j .bss -j .heap -j .stack -j .ro_mac_address -j .ro_ota_header -j .pad -S -O binary ${FILENAME} ${FILENAME}.tmp.bin
-        COMMAND "${SDK_PREFIX}\\Tools\\OTAUtils\\JET.exe" -m otamerge --embed_hdr -c ${FILENAME}.tmp.bin -v JN516x -n 1 -t 1 -u 0x1037 -o ${FILENAME}.bin
+        COMMAND "${SDK_PREFIX}\\Tools\\OTAUtils\\JET.exe" -m otamerge --embed_hdr -c ${FILENAME}.tmp.bin -v JN516x -n 1 -t 1 -u 0x1037 -j "HelloZigbee2021                 " -o ${FILENAME}.bin
     )
 ENDFUNCTION()
 
@@ -115,7 +115,7 @@ FUNCTION(ADD_OTA_BIN_TARGETS TARGET)
     ADD_CUSTOM_TARGET(${TARGET}.ota
         DEPENDS ${TARGET}.bin
 	# HACK/TODO: setting file version to 2 (-n 2), so that OTA image is always newer than current version
-        COMMAND "${SDK_PREFIX}\\Tools\\OTAUtils\\JET.exe" -m otamerge --embed_hdr -c ${FILENAME}.tmp.bin -v JN516x -n 2 -t 1 -u 0x1037 -o ${FILENAME}.bin
+        COMMAND "${SDK_PREFIX}\\Tools\\OTAUtils\\JET.exe" -m otamerge --embed_hdr -c ${FILENAME}.tmp.bin -v JN516x -n 2 -t 1 -u 0x1037 -j "HelloZigbee2021                 " -o ${FILENAME}.bin
         COMMAND "${SDK_PREFIX}\\Tools\\OTAUtils\\JET.exe" -m otamerge --ota -v JN516x -n 2 -t 1 -u 0x1037 -p 1 -c ${FILENAME}.bin -o ${FILENAME}.ota
     )
 ENDFUNCTION()
