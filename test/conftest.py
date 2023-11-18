6b716976d6ba3eae93c56df49976c89c76a2f7ab
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-11-18 17:58:15 +0200

    Improve getter and setter functions so that they can read different device attributes

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,44 @@
 import pytest
+import re
 
 def pytest_addoption(parser):
     parser.addini('port', 'COM port where the Zigbee Device is connected to')
     parser.addini('mqtt_server',    'IP or network name of the MQTT server')
     parser.addini('mqtt_port',      'MQTT server port')
     parser.addini('mqtt_topic',     'Base MQTT topic of the tested device')
+
+
+def set_device_attribute(device, zigbee, attribute, state, expected_response):
+    # Make json payload like {"state_button_3", "ON"}
+    payload = '{{"{}":"{}"}}'.format(attribute, state)
+
+    # Prepare for waiting a zigbee2mqtt message on the default device topic
+    zigbee.subscribe()
+
+    # Publish the 'set state' command
+    zigbee.publish('set', payload)
+
+    # Verify that the device has received the state change command
+    device.wait_str(expected_response)
+
+    # Wait the reply from zigbee2mqtt with the device state
+    state = zigbee.wait_msg()
+    return re.search(f'"{attribute}":"(.*?)"', state).group(1)
+
+
+def get_device_attribute(device, zigbee, attribute, expected_response):
+    # Make json payload like {"state_button_3", ""}
+    payload = f'{{"{attribute}":""}}'
+
+    # Prepare for waiting a zigbee2mqtt message on the default device topic
+    zigbee.subscribe()
+
+    # Request the device state
+    zigbee.publish('get', payload)                      
+
+    # Verify the device received read attribute command
+    device.wait_str(expected_response)
+
+    # Wait the reply from zigbee2mqtt with the device state
+    state = zigbee.wait_msg()
+    return re.search(f'"{attribute}":"(.*?)"', state).group(1)
