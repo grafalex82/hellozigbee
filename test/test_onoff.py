a9264811804f5c7a677e506018fc8aebff46b258
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-11-18 16:47:12 +0200

    Add possibility to read the device state

--- test/test_onoff.py
+++ test/test_onoff.py
@@ -3,22 +3,63 @@
 
 import pytest
 import time
+import re
 
 from device import *
 from zigbee import *
 
 
-def test_on_off(device, zigbee):
-    zigbee.publish('set', '{"state_button_2":"ON"}')
-    device.wait_str("SwitchEndpoint EP=3: do state change 1")
 
-    zigbee.publish('set', '{"state_button_2":"OFF"}')
-    device.wait_str("SwitchEndpoint EP=3: do state change 0")
+
+def set_device_state(device, zigbee, state, ep):
+    # Make json payload like {"state_button_3", "ON"}
+    channel = f"state_button_{ep-1}"
+    payload = '{{"{}":"{}"}}'.format(channel, state)    
+
+    # Prepare for waiting a zigbee2mqtt message on the default device topic
+    zigbee.subscribe()
+
+    # Publish the 'set state' command
+    zigbee.publish('set', payload)
+
+    # Verify that the device has received the state change command
+    device.wait_str(f"SwitchEndpoint EP={ep}: do state change")
+
+    # Wait the reply from zigbee2mqtt with the device state
+    state = zigbee.wait_msg()
+    return re.search(f'"{channel}":"(.*?)"', state).group(1)
+
+
+def get_device_state(device, zigbee, ep):
+    # Make json payload like {"state_button_3", ""}
+    channel = f"state_button_{ep-1}"
+    payload = f'{{"{channel}":""}}'                     
+
+    # Prepare for waiting a zigbee2mqtt message on the default device topic
+    zigbee.subscribe()
+
+    # Request the device state
+    zigbee.publish('get', payload)                      
+
+    # Verify the device received read attribute command
+    device.wait_str(f"ZCL Read Attribute: EP={ep} Cluster=0006 Command=00 Attr=0000")
+
+    # Wait the reply from zigbee2mqtt with the device state
+    state = zigbee.wait_msg()
+    return re.search(f'"{channel}":"(.*?)"', state).group(1)
+
+
+def test_on_off(device, zigbee):    
+    assert set_device_state(device, zigbee, 'ON', 3) == "ON"
+    assert set_device_state(device, zigbee, 'OFF', 3) == "OFF"
 
 
 def test_toggle(device, zigbee):
-    zigbee.publish('set', '{"state_button_2":"TOGGLE"}')
-    device.wait_str("SwitchEndpoint EP=3: do state change 1")
+    assert set_device_state(device, zigbee, 'OFF', 3) == "OFF"
+    assert get_device_state(device, zigbee, 3) == "OFF"
 
-    zigbee.publish('set', '{"state_button_2":"TOGGLE"}')
-    device.wait_str("SwitchEndpoint EP=3: do state change 0")
+    assert set_device_state(device, zigbee, 'TOGGLE', 3) == "ON"
+    assert get_device_state(device, zigbee, 3) == "ON"
+
+    assert set_device_state(device, zigbee, 'TOGGLE', 3) == "OFF"
+    assert get_device_state(device, zigbee, 3) == "OFF"
--- test/zigbee.py
+++ test/zigbee.py
@@ -1,20 +1,56 @@
 import pytest
+import time
 import paho.mqtt.client as mqtt
 
 
 class ZigbeeNetwork():
     def __init__(self, options):
-        self.client = mqtt.Client()
-        self.client.connect(options.getini('mqtt_server'), int(options.getini('mqtt_port')))
         self.topic = options.getini('mqtt_topic')
+        self.message_received = None
+
+        self.client = mqtt.Client()
+        self.client.on_message = self.on_message_received
+        self.client.connect(options.getini('mqtt_server'), int(options.getini('mqtt_port')))
+
+
+    def on_message_received(self, client, userdata, msg):
+        print(f"Received message on topic {msg.topic}: {msg.payload.decode()}")
+        self.message_received = msg.payload
+
+
+    def get_topic(self, subtopic):
+        if subtopic:
+            return self.topic + '/' + subtopic
+        
+        return self.topic
+
+
+    def subscribe(self, subtopic = ""):
+        self.client.subscribe(self.get_topic(subtopic))
+        self.message_received = None
 
 
     def publish(self, subtopic, message):
-        topic = self.topic + '/' + subtopic
-        print(f"Sending message to '{topic}'")
+        topic = self.get_topic(subtopic)
+        print(f"Sending message to '{topic}': {message}")
         self.client.publish(topic, message)
 
 
+    def wait_msg(self, timeout=5):
+        start_time = time.time()
+
+        while self.message_received is None:
+            self.client.loop(timeout=1)
+
+            elapsed_time = time.time() - start_time
+            if elapsed_time > timeout:
+                raise TimeoutError
+        
+        payload = self.message_received.decode()
+        self.message_received = None
+        return payload
+            
+            
     def disconnect(self):
         self.client.disconnect()
 
