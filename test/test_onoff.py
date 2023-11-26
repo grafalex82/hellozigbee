f683e7ba805c9e930cccdcbad73a05a4d8e754ff
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-11-26 18:28:55 +0200

    Run all tests for both button channels

--- test/test_onoff.py
+++ test/test_onoff.py
@@ -9,6 +9,13 @@ from zigbee import *
 from conftest import *
 
 class SmartSwitch:
+    """ 
+    Smart Switch Test Harness
+
+    This is a helper object that simplifies operating with the device via both UART and MQTT, performs routine checks,
+    and moves communication burden from the test to the test harness.
+    """
+
     def __init__(self, device, zigbee, ep, z2m_name):
         self.device = device
         self.zigbee = zigbee
@@ -102,16 +109,20 @@ class SmartSwitch:
         return self.zigbee.wait_msg()
 
 
-def test_on_off(device, zigbee):
-    switch = SmartSwitch(device, zigbee, 3, "button_2")
 
+# Make each test that uses switch fixture to run twice for both buttons. 
+# Using the ids parameter the button name will be displayed as a test parameter
+@pytest.fixture(params = [(2, "button_1"), (3, "button_2")], ids=lambda x: x[1])
+def switch(device, zigbee, request):
+    return SmartSwitch(device, zigbee, request.param[0], request.param[1])
+
+
+def test_on_off(switch):
     assert switch.switch('ON', True) == 'ON'
     assert switch.switch('OFF', False) == 'OFF'
 
 
-def test_toggle(device, zigbee):
-    switch = SmartSwitch(device, zigbee, 3, "button_2")
-
+def test_toggle(switch):
     assert switch.switch('OFF', False) == 'OFF'
     assert switch.get_state() == 'OFF'
 
@@ -123,32 +134,24 @@ def test_toggle(device, zigbee):
 
 
 @pytest.mark.parametrize("switch_mode", ["toggle", "momentary", "multifunction"])
-def test_oosc_attribute_switch_mode(device, zigbee, switch_mode):
-    switch = SmartSwitch(device, zigbee, 3, "button_2")
-
+def test_oosc_attribute_switch_mode(switch, switch_mode):
     assert switch.set_attribute('switch_mode', switch_mode) == switch_mode
     assert switch.get_attribute('switch_mode') == switch_mode
 
 
 @pytest.mark.parametrize("switch_actions", ["onOff", "offOn", "toggle"])
-def test_oosc_attribute_switch_action(device, zigbee, switch_actions):
-    switch = SmartSwitch(device, zigbee, 3, "button_2")
-
+def test_oosc_attribute_switch_action(switch, switch_actions):
     assert switch.set_attribute('switch_actions', switch_actions) == switch_actions
     assert switch.get_attribute('switch_actions') == switch_actions
 
 
 @pytest.mark.parametrize("relay_mode", ["unlinked", "front", "single", "double", "tripple", "long"])
-def test_oosc_attribute_relay_mode(device, zigbee, relay_mode):
-    switch = SmartSwitch(device, zigbee, 3, "button_2")
-
+def test_oosc_attribute_relay_mode(switch, relay_mode):
     assert switch.set_attribute('relay_mode', relay_mode) == relay_mode
     assert switch.get_attribute('relay_mode') == relay_mode
 
 
-def test_oosc_attributes_survive_reboot(device, zigbee):
-    switch = SmartSwitch(device, zigbee, 3, "button_2")
-
+def test_oosc_attributes_survive_reboot(switch):
     # Set a specific OOSC options
     assert switch.set_attribute('switch_mode', 'multifunction') == 'multifunction'
     assert switch.set_attribute('relay_mode', 'double') == 'double'
@@ -167,9 +170,7 @@ def test_oosc_attributes_survive_reboot(device, zigbee):
     assert switch.get_attribute('min_long_press') == 602
 
 
-def test_btn_press(device, zigbee):
-    switch = SmartSwitch(device, zigbee, 3, "button_2")
-
+def test_btn_press(switch):
     # Ensure the switch is off on start, and the mode is 'toggle'
     assert switch.switch('OFF', False) == 'OFF'
     assert switch.set_attribute('switch_mode', 'toggle') == 'toggle'
@@ -186,13 +187,11 @@ def test_btn_press(device, zigbee):
     switch.wait_button_state("IDLE")
 
     # Check the device state changed, and the action is generated (in this particular order)
-    assert switch.wait_zigbee_state()['action'] == "single_button_2"
-    assert switch.wait_zigbee_state()['state_button_2'] == "ON"
+    assert switch.wait_zigbee_state()['action'] == "single_" + switch.z2m_name
+    assert switch.wait_zigbee_state()['state_' + switch.z2m_name] == "ON"
 
 
-def test_double_click(device, zigbee):
-    switch = SmartSwitch(device, zigbee, 3, "button_2")
-
+def test_double_click(switch):
     # Ensure the switch is off on start, the mode is 'multifunction', and relay mode is 'double'
     assert switch.switch('OFF', False) == 'OFF'
     assert switch.set_attribute('switch_mode', 'multifunction') == 'multifunction'
@@ -214,15 +213,13 @@ def test_double_click(device, zigbee):
     switch.wait_state_change_msg(True)
 
     # Check the device state changed, and the double click action is generated
-    assert switch.wait_zigbee_state()['action'] == "double_button_2"
-    assert switch.wait_zigbee_state()['state_button_2'] == "ON"
+    assert switch.wait_zigbee_state()['action'] == "double_" + switch.z2m_name
+    assert switch.wait_zigbee_state()['state_' + switch.z2m_name] == "ON"
 
 
-def test_level_control(device, zigbee):
-    switch = SmartSwitch(device, zigbee, 3, "button_2")
-
+def test_level_control(switch):
     # Bind the endpoint with the coordinator
-    send_bind_request(zigbee, "genLevelCtrl", "my_test_switch/3", "Coordinator")
+    send_bind_request(switch.zigbee, "genLevelCtrl", f"my_test_switch/{switch.ep}", "Coordinator")
     
     # Ensure the switch will generate levelCtrlDown messages on long press
     assert switch.set_attribute('switch_mode', 'multifunction') == 'multifunction'
@@ -237,7 +234,7 @@ def test_level_control(device, zigbee):
     switch.wait_report_level_ctrl("Move")
 
     # Verify the Level Control Move command has been received by the coordinator
-    assert switch.wait_zigbee_state()['action'] == "hold_button_2"
+    assert switch.wait_zigbee_state()['action'] == "hold_" + switch.z2m_name
     assert switch.wait_zigbee_state()['level_ctrl'] == {'command': 'commandMove', 'payload': {'movemode': 1, 'rate': 80}}
 
     # Do not forget to release the button
@@ -247,5 +244,5 @@ def test_level_control(device, zigbee):
     switch.wait_report_level_ctrl("Stop")
 
     # Verify the Level Control Move command has been received by the coordinator
-    assert switch.wait_zigbee_state()['action'] == "release_button_2"
+    assert switch.wait_zigbee_state()['action'] == "release_" + switch.z2m_name
     assert switch.wait_zigbee_state()['level_ctrl']['command'] == 'commandStop'
