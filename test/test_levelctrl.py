57d0e21555d129c3070bba649682682f3c21b40d
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-12-29 20:41:11 +0200

    Add more LevelCtrl tests

--- test/test_levelctrl.py
+++ test/test_levelctrl.py
@@ -4,9 +4,38 @@ def test_on(switch):
     # start with switch is OFF
     switch.switch('OFF')
 
-    # Turn the switch ON, and check it sets non-zero brightness level
+    # Turn the switch ON via On/Off cluster, and check it sets non-zero brightness level
     state, level = switch.switch('ON')
     assert level != 0
+
+
+def test_on_toggle(switch):
+    # start with switch is OFF
+    switch.switch('OFF')
+
+    # Turn the switch ON via On/Off cluster, and check it sets non-zero brightness level
+    state, level = switch.switch('TOGGLE')
+    assert state == "ON"
+    assert level != 0
+
+
+def test_off(switch):
+    # start with switch is ON
+    switch.switch('ON')
+
+    # Turn the switch off using OFF On/Off cluster command, and check the device switches off
+    state, level = switch.switch('OFF')
+    assert level == 0
+
+
+def test_off_toggle(switch):
+    # start with switch is ON
+    switch.switch('ON')
+
+    # Turn the switch OFF using TOGGLE command, and check it switches device off
+    state, level = switch.switch('TOGGLE')
+    assert state == "OFF"
+    assert level == 0
 
 
 def test_off_set_level(switch):
@@ -17,6 +46,16 @@ def test_off_set_level(switch):
     state, level = switch.set_level(123)
     assert state == "ON"
     assert level == 123
+
+
+def test_off_set_zero_level(switch):
+    # start with switch is OFF
+    switch.switch('OFF')
+
+    # Set the zero brightness level, and verify the switch is still off
+    state, level = switch.set_level(0)
+    assert state == "OFF"
+    assert level == 0
 
 
 def test_on_set_level(switch):
