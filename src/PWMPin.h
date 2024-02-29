bdc6229c8d9201ff5e5acf10292c39c8906eec33
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2024-02-29 20:23:32 +0200

    Add possibility to use alternative pins for PWM

--- src/PWMPin.h
+++ src/PWMPin.h
@@ -10,11 +10,13 @@ extern "C"
 const uint8 PWM_MAX = 255;
 
 // Use the following table to map timerIDs to a pin number
-// - E_AHI_TIMER_0 - 10
-// - E_AHI_TIMER_1 - 11
-// - E_AHI_TIMER_2 - 12
-// - E_AHI_TIMER_3 - 13
-// - E_AHI_TIMER_4 - 17
+// - E_AHI_TIMER_0 - 10 (Alternative pin - 4)
+// - E_AHI_TIMER_1 - 11 (Alternative pin - 5)
+// - E_AHI_TIMER_2 - 12 (Alternative pin - 6)
+// - E_AHI_TIMER_3 - 13 (Alternative pin - 7)
+// - E_AHI_TIMER_4 - 17 (Alternative pin - 8)
+//
+// To use alternative pin pass timerId | 0x80
 
 class PWMPin
 {
@@ -29,10 +31,15 @@ public:
 
     void init(uint8 timer)
     {
-        timerId = timer;
+        timerId = timer & 0x7f;
+        bool useAltPins = timer & 0x80;
 
         vAHI_TimerEnable(timerId, 6, FALSE, FALSE, TRUE);
         vAHI_TimerConfigureOutputs(timerId, TRUE, TRUE);
+
+        if(useAltPins)
+            vAHI_TimerSetLocation(timerId, TRUE, FALSE);
+
         setLevel(0);
     }
 
